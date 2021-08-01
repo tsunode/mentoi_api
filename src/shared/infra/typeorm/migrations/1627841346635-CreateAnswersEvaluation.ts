d61672c377ba8dd2caffa1e333f2e37cc8b31cd7
tsunodagabriel <gabriel.tsunoda@vitta.me>
2021-08-01 15:13:25 -0300

    feat: create answer evaluation table

---
+++ src/shared/infra/typeorm/migrations/1627841346635-CreateAnswersEvaluation.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateAnswersEvaluation1627841346635
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'answers_evaluation',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'answer_id',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'type',
+            type: 'enum',
+            enum: ['PERFECT', 'GOOD', 'BAD'],
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'UseranswersEvaluation',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['USER_ID'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'AnswersEvaluation',
+            referencedTableName: 'answers',
+            referencedColumnNames: ['id'],
+            columnNames: ['answer_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('answers_evaluation');
+  }
+}
